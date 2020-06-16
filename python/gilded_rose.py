63587176c2601d34db7fb4c41bac75141cd3b421
Jean Nassar <jean.nassar@yields.io>
2020-06-17 01:35:16 +0200

    Use subclasses instead of ifs.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,41 +1,14 @@
 # -*- coding: utf-8 -*-
+from typing import List
+
+
 class GildedRose:
-    categories = {
-        "Magical foo": "other",
-        "Aged Brie": "brie",
-        "Backstage passes to a TAFKAL80ETC concert": "passes",
-        "Sulfuras, Hand of Ragnaros": "sulfuras",
-        "Conjured Mana Cake": "conjured",
-    }
-
-    changes = {
-        "passes": 1,
-        "brie": 1,
-        "conjured": -2,
-        "other": -1,
-    }
-
-    def __init__(self, items):
+    def __init__(self, items: List["Category"]):
         self.items = items
-
-    @staticmethod
-    def _clip(value, min_=0, max_=50):
-        return min(max(value, min_), max_)
 
     def update_quality(self):
         for item in self.items:
-            category = self.categories[item.name]
-            if category == "sulfuras":
-                continue
-            change = self.changes.get(category)
-            if category == "passes":
-                if item.sell_in <= 0:
-                    item.quality = 0
-                    continue
-                multiplier = max(1, (20 - item.sell_in) // 5)
-            else:
-                multiplier = 1 if item.sell_in >= 1 else 2
-            item.quality = self._clip(item.quality + change * multiplier)
+            item.update_quality()
 
 
 class Item:
@@ -46,3 +19,56 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class Category(Item):
+    def __init__(self, name, sell_in, quality, *, min_quality=0, max_quality=50, change=-1):
+        super().__init__(name, sell_in, quality)
+        self.min_quality = min_quality
+        self.max_quality = max_quality
+        self.change = change
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, sell_in={self.sell_in}, quality={self.quality})"
+
+    @property
+    def multiplier(self):
+        return 1 if self.sell_in > 0 else 2
+
+    def _clip_quality(self, value):
+        return min(max(value, self.min_quality), self.max_quality)
+
+    def _next_quality(self):
+        return self._clip_quality(self.quality + self.change * self.multiplier)
+
+    def update_quality(self):
+        self.quality = self._next_quality()
+
+
+class Brie(Category):
+    def __init__(self, name, sell_in, quality, change=1):
+        super().__init__(name, sell_in, quality, change=change)
+
+
+class Conjured(Category):
+    def __init__(self, name, sell_in, quality, change=-2):
+        super().__init__(name, sell_in, quality, change=change)
+
+
+class Passes(Category):
+    def __init__(self, name, sell_in, quality, change=1):
+        super().__init__(name, sell_in, quality, change=change)
+
+    def _next_quality(self):
+        if self.sell_in <= 0:
+            return 0
+        return super()._next_quality()
+
+    @property
+    def multiplier(self):
+        return max(1, (20 - self.sell_in) // 5)
+
+
+class Sulfuras(Category):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality, min_quality=80, max_quality=80, change=0)
